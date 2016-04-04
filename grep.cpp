1083c980fa155b9db1e2a01e7c92bcbb7f35d388
Nitesh Agrawal <nkagrawal14@gmail.com>
2016-04-04 11:02:37 +0530

    code edited

--- grep.cpp
+++ grep.cpp
@@ -1,36 +1,30 @@
-#include<bits/stdc++.h>
-#include <dirent.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-	string dn,pat,temp;
-	string word;
-	cout<<"Enter directory name"<<endl;
-	cin>>dn;
-	cout<<" ENter the pattern"<<endl;
-	cin>>pat;
-	DIR *dir;
-	struct dirent *ent;
-	if ((dir = opendir(dn.c_str())) != NULL) {
-	  while ((ent = readdir (dir)) != NULL) {
-		ifstream input;
-		input.open(ent->d_name);
-		if(ent->d_name[0] == '.')
-			continue;
-		while(getline(input,temp))
+	FILE *fp;
+	int line=1,col=1;
+	char ch;
+	string str,in;
+	cout << "enter search string" << endl;
+	cin >> in;
+	fp = fopen("data/ap880212.txt","r");
+	do
+	{
+		ch = fgetc(fp);
+		if(ch=='\n')
 		{
-			istringstream str(temp);
-			while(str>>word)
-			{
-				if(word==pat)
-				cout<<"FILE "<<ent->d_name<<" "<<temp<<endl;
-			}
+			line++;
+			col=1;
 		}
-		input.close();
-		input.clear();
-	  }
-	  closedir (dir);
-	}
-	
-	return 1;
+		if(ch==' ')
+			str="";
+		else
+			str=str+ch;
+		if(str==in)
+			cout << line << " " << col-in.size() << endl;
+		col++;
+	}while(!feof(fp));
+	fclose(fp);
+	return 0;
 }
